7cdce650dc738113d0dda135f658390bfec3a17d
ajaidanial <ajaidanial@gmail.com>
2019-06-30 19:45:29 +0530

    theme test | 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Routes from './Routes';
+import './theme/dist/css/style.css'
 
 const browserHistory = createBrowserHistory();
 
